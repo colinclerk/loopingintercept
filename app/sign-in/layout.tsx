ac7a76855e358436f48819d711bc8e8fc7c33fc6
colinclerk <colin@clerk.dev>
2023-04-07 20:43:59 -0700

    looping error

--- app/sign-in/layout.tsx
+++ app/sign-in/layout.tsx
@@ -1,4 +1,4 @@
-import "./globals.css";
+import "../globals.css";
 
 import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
@@ -10,17 +10,12 @@ export const metadata = {
 
 export default function RootLayout({
   children,
-  authmodal,
 }: {
   children: React.ReactNode;
-  authmodal: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <div>Auth modal: {authmodal}</div>
-        {children}
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
